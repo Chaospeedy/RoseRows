8034aa6dd4949f25a5c4c51dd451240406c45427
Sean <seansdilday@gmail.com>
2025-11-14 23:22:32 -0600

    button testing

---
+++ Assets/Scripts/ClickScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+
+    public GameObject go;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown() {
+        go.SetActive(true);
+    }
+    
+}
